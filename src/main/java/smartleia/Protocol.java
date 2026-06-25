b0654bf288224db76c170ae79a82005c81560e3b
Veronika Hanulikova <xhanulik@gmail.com>
2026-06-25 16:30:12 +0200

    feat: add enum for protocol setting

---
+++ src/main/java/smartleia/Protocol.java
@@ -0,0 +1,34 @@
+// SPDX-FileCopyrightText: 2019 The LEIA Team <leia@ssi.gouv.fr>
+// SPDX-FileCopyrightText: 2026 Veronika Hanulikova <xhanulik@gmail.com>
+// SPDX-License-Identifier: BSD-3-Clause
+
+/**
+ * This file is derived from the SmartLEIA project
+ * (https://github.com/cw-leia/smartleia),
+ * originally developed by the LEIA Team and licensed under the BSD 3-Clause
+ * License.
+ *
+ * Modifications and translation by Veronika Hanulikova.
+ */
+
+package smartleia;
+
+/** ISO 7816 communication protocol to use when configuring the smartcard. */
+public enum Protocol {
+    /** Let the board auto-detect the protocol (not currently supported by the driver). */
+    AUTO((byte) 0),
+    /** ISO 7816 T=0 (character-oriented). */
+    T0((byte) 1),
+    /** ISO 7816 T=1 (block-oriented). */
+    T1((byte) 2);
+
+    private final byte value;
+
+    Protocol(byte value) {
+        this.value = value;
+    }
+
+    byte value() {
+        return value;
+    }
+}
